cec06386b4677c96e76af61c27d1d7b0073efedc
Carlos Racca <carlosracca@MacBook-Pro-de-Carlos.local>
2025-05-14 20:14:50 -0300

    Se agregan cosas para que ande la conexion a la DB

--- backend/server.js
+++ backend/server.js
@@ -40,26 +40,6 @@ app.get('/api/ping', (req, res) => {
 // Usar las rutas del formulario de contacto
 app.use('/api/contact', contactRoutes);
 
-app.post('/api/contact', async (req, res) => {
-  try {
-    const { name, email, message } = req.body;
-
-    console.log('Datos recibidos:', { name, email, message });
-
-    const result = await pool.query(
-      'INSERT INTO contacts (name, email, message) VALUES ($1, $2, $3) RETURNING *',
-      [name, email, message]
-    );
-
-    console.log('Resultado DB:', result.rows[0]);
-
-    res.status(200).json({ success: true, data: result.rows[0] });
-  } catch (error) {
-    console.error('❌ ERROR INTERNO:', error);
-    res.status(500).json({ success: false, error: "Error interno del servidor" });
-  }
-});
-
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
